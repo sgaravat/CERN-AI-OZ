250974916578e049cd2ac1729f80c18a4f17d17f
Massimo Sgaravatto <massimo.sgaravatto@gmail.com>
2013-11-21 14:17:56 +0100

    Repo setup

---
+++ README.md
@@ -0,0 +1,2 @@
+tdl oz and relevant files needed to create an image for CMS activities on
+CERN-Agile Infrastructure
---
+++ runme.sh
@@ -0,0 +1,2 @@
+oz-install -d 4 -t 7200 -u -a kickstart -p cern-ai-glideinwms-glexec.xml
+#oz-install -d 4 -t 7200 -u -a kickstart -f -p cern-ai-glideinwms-glexec.xml
